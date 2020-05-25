73165e302ba3c6a9fd021d1ecc2371d4a1bed6f8
sarah-bell88 <50850947+sarah-bell88@users.noreply.github.com>
2020-05-24 20:32:07 -0700

    Add files via upload

--- connect.php
+++ connect.php
@@ -6,7 +6,7 @@
     //database connection
 
     $dbhost = 'oniddb.cws.oregonstate.edu';
-    $dbname = 'robin-select';
+    $dbname = 'bellsar-db';
     $dbuser = 'bellsar-db';
     $dbpass = 'CFmzSBN8s5dvCl5x';
 
@@ -14,7 +14,7 @@
     if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     } else {
-        $stmt = $conn->prepare("insert into vote(name, robin, comment)
+        $stmt = $conn->prepare("insert into robin-select(name, robin, comment)
         values(?,?,?)");
         $stmt->bind_param("sss", $name, $robin, $comment);
         $stmt->execute();
